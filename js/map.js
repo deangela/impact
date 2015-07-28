af3974478e12a78efa42cf588cf285bb037d070f
Anneka Goss <annekagoss@gmail.com>
2015-07-27 20:46:59 -0400

    replaced with d3 version

--- js/map.js
+++ js/map.js
@@ -1,213 +1,85 @@
-var map;
-function initialize() {
-	var mapOptions = {
-		center: { lat: 20, lng: 0},
-		zoom: 2,
-		mapTypeControl: false,
-		streetViewControl: false,
-		mapTypeControl: false,
-    zoomControl: true,
-    zoomControlOptions: {
-      style: google.maps.ZoomControlStyle.SMALL,
-      position: google.maps.ControlPosition.BOTTOM_CENTER
-    },
-    panControl: true,
-    panControlOptions: {
-      position: google.maps.ControlPosition.BOTTOM_CENTER,
+var first_layer = 'd3_world_borders';
+    var second_layer = 'd3_populated_places';
+
+    var sql = new cartodb.SQL({ user: 'viz2', format: 'geojson', dp: 5});
+
+    // Define our SVG element outside our SQL functions
+    var svg = d3.select("#map")
+            .append("svg")
+                .style("width", "100%")
+                .style("height", "100%")
+                .style("background", "white")
+            // .call(d3.behavior.zoom()
+            //     .on("zoom", redraw))
+            .append("g");
+
+    var mapMarginTop = 175;
+        mapMarginLeft = 155;
+
+    // Our projection.
+    var xy = d3.geo.mercator().scale(5);
+        xy.scale(225);
+        xy.center([0,0]);
+
+    svg.append("g").attr("id", "first_layer");
+    svg.append("g").attr("id", "second_layer");
+
+    var path = d3.geo.path();
+
+    sql.execute("SELECT ST_Simplify(the_geom,0.01) as the_geom FROM {{table_name}} WHERE the_geom IS NOT NULL", {table_name: first_layer})
+      .done(function(collection) {
+          svg.select("#first_layer")
+            .selectAll("path")
+              .data(collection.features)
+            .enter().append("path")
+            .attr("d", path.projection(xy))
+            .attr("transform", "translate("  + mapMarginLeft + "," + mapMarginTop + ")");
+
+              
+      })
+      .error(function(errors) {
+        // console.log('Errors! Oh no!')
+      });
+
+
+      sql.execute("SELECT the_geom, 4/sqrt(scalerank) as pop, adm1name as name FROM {{table_name}} WHERE the_geom IS NOT NULL AND pop_max > 10^5", {table_name: second_layer})
+        .done(function(collection) {
+
+        var label = svg.append("sgv:g")
+
+        svg.select("#second_layer")
+            .selectAll("path")
+            .data(collection.features)
+            .enter()
+            .append("circle")
+            .attr("r", function(d){ return d.properties.pop })
+          .attr("cy", function(){ return xy([0,Math.random()*180.0-90])[1]})
+          .attr("cx", function(){ return xy([Math.random()*360.0-180,0])[0]})
+          .attr("d", path.projection(xy))
+            .on("mousedown", function(t){
+                d3.selectAll("text").remove(); 
+              label.append("svg:text")
+                .attr("x", xy(t.geometry.coordinates)[0])
+                .attr("y", xy(t.geometry.coordinates)[1])
+                .attr("class", "text")
+                .text(t.properties.name)
+                .attr("transform", "translate("  + mapMarginLeft + "," + mapMarginTop + ")");
+              })
+            .on("mouseout", function(){
+              })
+          .transition()
+          .attr("cy", function(d){ return xy(d.geometry.coordinates)[1] })
+          .attr("cx", function(d){ return xy(d.geometry.coordinates)[0] })
+          .duration(function(d){ return 800*d.properties.pop })
+          .delay(100) 
+          .attr("transform", "translate("  + mapMarginLeft + "," + mapMarginTop + ")");
+        })
+
+        .error(function(errors) {
+          // console.log('Errors! Oh no!')
+        });
+
+    function redraw() {
+      svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+      // svg.attr("transform", "translate(" + 200 + ")scale(" + d3.event.scale + ")");
     }
-
-
-
-  };
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
-
-  var infowindow = new google.maps.InfoWindow({
-    content: '',
-    // position: { lat: 20, lng: 0},
-  });
-  infowindow.close(map);
-
-
-
-
-  var styles = [
-
-  {
-    stylers: [
-      //{ hue: "#ffffff" },
-      { saturation: -100 }
-      ]
-    },{
-     featureType: "road",
-     elementType: "all",
-     stylers: [
-     { visibility: "off" }
-     ]
-   },{
-     featureType: "administrative",
-     elementType: "geometry.fill",
-     stylers: [
-     { visibility: "off" }
-     ]
-   },{
-     featureType: "administrative",
-     elementType: "labels",
-     stylers: [
-     {visibility: "off"}
-     ]
-   },{
-     featureType: "administrative",
-     elementType: "geometry.stroke",
-     stylers: [
-     {color: "#dedede"}
-     ]
-   },{
-     featureType: "administrative.province",
-     elementType: "all",
-     stylers: [
-     { visibility: "off" }
-     ]
-   },{
-     featureType: "transit",
-     elementType: "all",
-     stylers: [
-     { visibility: "off" }
-     ]
-   },{
-     featureType: "water",
-     elementType: "geometry.fill",
-     stylers: [
-     {color: "#000000"}
-     ]
-   },{
-     featureType: "water",
-     elementType: "labels",
-     stylers: [
-     {visibility: "off"}
-     ]
-   },{
-     featureType: "poi",
-     elementType: "all",
-     stylers: [
-     {visibility: "off"}
-     ]
-   },{
-     featureType: "landscape",
-     elementType: "labels",
-     stylers: [
-     {visibility: "off"}
-     ]
-   },{
-     featureType: "landscape",
-     elementType: "geometry",
-     stylers: [
-     {lightness: 60}
-     ]
-   }
-   ];
-
-   map.setOptions({styles: styles});
-
-
-
-
-   // map.data.loadGeoJson('https://storage.googleapis.com/maps-devrel/google.json');
-   var data = {
-    "type": "FeatureCollection",
-    "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        "name": "Aachen",
-        "year": "1880",
-        "location": "Nordrhein-Westfalen, Germany",
-        "mass": "21 g",
-        "metType": "L5",
-        "lat": 50.775000,
-        "long": 6.083330
-      },
-      "geometry": {
-        "type": "Point", "coordinates": [6.083330, 50.775000]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "name": "Aarhus",
-        "year": "1951",
-        "location": "Region Midtjylland, Denmark",
-        "mass": "720 g",
-        "metType": "H6",
-        "lat": 56.183330,
-        "long": 10.233330
-      },
-      "geometry": {
-        "type": "Point", "coordinates": [10.233330, 56.183330]
-      }
-    }
-    ]
-  };
-
-  map.data.addGeoJson(data);
-
-   // Set mouseover event for each feature.
-   map.data.addListener('mouseover', function(event) {
-    infowindow.open(map);
-    var infoName = String(event.feature.getProperty('name'));
-    var infoLocation = String(event.feature.getProperty('location'));
-    var infoYear = String(event.feature.getProperty('year'));
-    var infoMass = String(event.feature.getProperty('mass'));
-    var infoType = String(event.feature.getProperty('metType'));
-    var information = infoName.concat("<br>" + infoLocation + "<br>" + infoYear + "<br>" + infoMass + "<br>" + infoType);
-    infowindow.setContent(information);
-    var infoLat = event.feature.getProperty('lat') + 10;
-    var infoLong = event.feature.getProperty('long');
-    infowindow.setPosition({ lat: infoLat, lng: infoLong });
-  });
-
-   map.data.addListener('mouseout', function(event) {
-    infowindow.close(map);
-  });
-
-
-
- }
-
-
- google.maps.event.addDomListener(window, 'load', initialize);
-		// google.maps.event.addDomListener(window, "resize", function() {
-		// 	var center = map.getCenter();
-		// 	google.maps.event.trigger(map, "resize");
-		// 	map.setCenter(center); 
-		// });
-
-
-
-//INFO BOX HOVER
-$(function() {
-  $('.hover').hover(function(){
-    var popup_div = $('#info-box');
-    var obj = $(this);
-    var offset = obj.offset();
-
-    var new_top = offset.top + 30;
-
-    var new_left = offset.left;
-
-    new_left = new_left - ( popup_div.width() / 2);
-    new_left = new_left + ( obj.width() / 2);
-
-    popup_div.css('left', new_left + 'px');
-    popup_div.css('top', new_top + 'px');
-
-    popup_div.show();
-  }
-  , function (){
-        //hovered away so hide popup
-        $('#info-box').hide();
-      }
-      );
-});
-
